f4aa2abebee7277d30437def55fc2e6fd1c88306
Dipan Roy <38533808+dipan29@users.noreply.github.com>
2020-05-18 01:31:25 +0530

    Angular Stopwatch Working

--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -10,15 +10,15 @@ import { Component, OnInit } from '@angular/core';
 export class AppComponent implements  OnInit {
   title = 'My Timer';
 
-  paused: any = 0;
+  paused: any = null;
   startTime: any;
   endTime: any;
   lastUpdated: any = new Date().getTime();
   timeElapsed: any = 0;
 
-  hours: any;
-  minutes: any;
-  seconds: any;
+  hours: any = 0;
+  minutes: any = 0;
+  seconds: any = 0;
 
   ngOnInit(){
     this.hours = ('00' + 0).substr(-2);
@@ -26,11 +26,11 @@ export class AppComponent implements  OnInit {
     this.seconds = ('00' + 0).substr(-2);
   }
 
-  startTimer(){
-    if(!this.paused){
+  startTimer(): void{
+    if(this.paused == null || this.paused == 0){
       this.lastUpdated = new Date().getTime();
-      this.paused = setInterval(this.update, 1);
-    }
+      this.paused = setInterval(() => this.update(), 1);
+    } 
   }
   
   pauseTimer(){
@@ -41,34 +41,35 @@ export class AppComponent implements  OnInit {
           // set stop cookie = 1; 
       } else {
           this.lastUpdated = new Date().getTime();
-          this.paused = setInterval(this.update, 1);
+          this.paused = setInterval(() => this.update(), 1);
           // Change the cookie & set stop = 0
       }
   }
 
   resetTimer(){
     clearInterval(this.paused);
-        this.paused = 0;
-        this.timeElapsed = 0;
+    this.paused = 0;
+    this.timeElapsed = 0;
+    this.hours = this.minutes = this.seconds = "00";
   }
 
-  update(){
+  update(): void {
     var now = new Date().getTime();
     var dt = now - this.lastUpdated;
-    this.timeElapsed += dt;
-
+    this.timeElapsed = (this.timeElapsed || 0) + dt;
+    // console.log(this.timeElapsed);
     let val = new Date(this.timeElapsed);
+    val.setHours(val.getHours()); 
+    val.setMinutes(val.getMinutes());
     val.setHours(val.getHours() - 5); 
     val.setMinutes(val.getMinutes() - 30);
-    console.log(val);
-    // this.hrs = val.getHours();
-    // this.mins = val.getMinutes();
-    // this.secs = val.getSeconds();
+    // console.log(val.getSeconds());
 
-    this.hours = val.getHours();
-    this.minutes = val.getMinutes();
-    this.seconds = val.getSeconds();
+    this.hours = ('00' + val.getHours()).substr(-2);
+    this.minutes = ('00' + val.getMinutes()).substr(-2);
+    this.seconds = ('00' + val.getSeconds()).substr(-2);
 
     this.lastUpdated = now;
   }
+  
 }
