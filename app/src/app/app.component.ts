84ce30845abb47f51b83905bcc9bb5200bd14ad1
Ayushman Bilas Thakur <ayushmanbilasthakur@gmail.com>
2020-05-23 02:15:02 +0530

    Some new CSS

--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -4,20 +4,35 @@ import { generate } from 'shortid';
 import { ActivityListItem } from './activity-list-item';
 // import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-
-export class AppComponent implements  OnInit, OnDestroy {
+export class AppComponent implements OnInit, OnDestroy {
   title = 'My Timer';
 
-  myColours: string[] = ["#1abc9c", "#16a085", "#2ecc71", "#27ae60", "#3498db", "#2980b9", "#9b59b6", "#8e44ad", "#34495e", "#2c3e50", "#f1c40f", "#f39c12", "#e67e22", "#d35400", "#e74c3c", "#c0392b"];
+  myColours: string[] = [
+    '#1abc9c',
+    '#16a085',
+    '#2ecc71',
+    '#27ae60',
+    '#3498db',
+    '#2980b9',
+    '#9b59b6',
+    '#8e44ad',
+    '#34495e',
+    '#2c3e50',
+    '#f1c40f',
+    '#f39c12',
+    '#e67e22',
+    '#d35400',
+    '#e74c3c',
+    '#c0392b',
+  ];
   color_set1: string[] = this.myColours;
 
-  paused: any = null;  // cookie
+  paused: any = null; // cookie
   startTime: any;
   endTime: any;
   lastUpdated: any = new Date().getTime();
@@ -25,8 +40,8 @@ export class AppComponent implements  OnInit, OnDestroy {
   state: any = 0;
   changed: boolean = false;
   reset: number = 1;
-  reset_text: string = "STOP";
-  start_text: string = "START";
+  reset_text: string = 'STOP';
+  start_text: string = 'START';
 
   hours: any = 0;
   minutes: any = 0;
@@ -36,26 +51,23 @@ export class AppComponent implements  OnInit, OnDestroy {
   // lap_name: any = '';
 
   add_placeholder: string = 'Your Task';
-  public tag_name : string = '';
+  public tag_name: string = '';
   tags: any = [];
   activities: ActivityListItem[] = [];
 
-  constructor(
-    private cookieService: CookieService
-  ) {}
+  constructor(private cookieService: CookieService) {}
 
-  public ngOnInit(): void{
-
+  public ngOnInit(): void {
     this.cookieCheck();
 
     this.initiate(this.hours, this.minutes, this.seconds);
 
     var json_str = this.cookieService.get('main-laps');
-    if(json_str.length > 1){
+    if (json_str.length > 1) {
       let laps = JSON.parse(json_str);
       this.laps = laps;
     }
-    if(this.state == 1){
+    if (this.state == 1) {
       this.paused = 0;
       this.changed = true;
       this.startTimer();
@@ -63,118 +75,125 @@ export class AppComponent implements  OnInit, OnDestroy {
     this.fetchTimerState();
   }
 
-  public ngOnDestroy(): void{
+  public ngOnDestroy(): void {
     // this.saveTimerState();
   }
 
-  startTimer(): void{
-    if(this.reset_text == "RESET" && this.reset == 0){
+  startTimer(): void {
+    if (this.reset_text == 'RESET' && this.reset == 0) {
       this.resetTimer();
-      this.start_text = "START OVER";
+      this.start_text = 'START OVER';
     }
 
-    if(this.paused == null || this.paused <= 0){
-      if(!this.changed)
-        this.lastUpdated = new Date().getTime();
-        
-      this.activities.forEach(activity => {
-        if(activity.lastState){
+    if (this.paused == null || this.paused <= 0) {
+      if (!this.changed) this.lastUpdated = new Date().getTime();
+
+      this.activities.forEach((activity) => {
+        if (activity.lastState) {
           activity.startTimer();
         }
       });
       this.state = 1;
-      this.reset_text = "STOP";
+      this.reset_text = 'STOP';
       this.reset = 0;
       this.paused = setInterval(() => this.update(), 100);
-      this.start_text = "START";
-    } 
-  }
-  
-  pauseTimer(){
-    var t = this.paused;
-    this.activities.forEach(activity => {
-      if(activity.getState())
-        activity.pauseTimer();
-    });
-      if(t) {
-          clearInterval(this.paused);
-          this.paused = -1;
-          this.state = 0;
-          this.setCookies();
-          // set stop cookie = 1; 
-      } else {
-          this.lastUpdated = new Date().getTime();
-          this.state = 1;
-          this.paused = setInterval(() => this.update(), 100);
-          // Change the cookie & set stop = 0
-          this.setCookies();
-      }
+      this.start_text = 'START';
+    }
   }
 
-  resetTimer(){
+  pauseTimer() {
+    var t = this.paused;
+    this.activities.forEach((activity) => {
+      if (activity.getState()) activity.pauseTimer();
+    });
+    if (t) {
+      clearInterval(this.paused);
+      this.paused = -1;
+      this.state = 0;
+      this.setCookies();
+      // set stop cookie = 1;
+    } else {
+      this.lastUpdated = new Date().getTime();
+      this.state = 1;
+      this.paused = setInterval(() => this.update(), 100);
+      // Change the cookie & set stop = 0
+      this.setCookies();
+    }
+  }
+
+  resetTimer() {
+    if (this.reset === 1) return;
+
     clearInterval(this.paused);
     this.paused = 0;
     this.timeElapsed = 0;
     this.state = 0;
-    this.activities.forEach(activity => {
+    this.activities.forEach((activity) => {
       activity.resetTimer();
     });
-    if(this.reset_text == "RESET" && this.reset == 0){
+    this.start_text = 'START OVER';
+
+    if (this.reset_text == 'RESET' && this.reset == 0) {
       this.laps = [];
       this.reset = 1;
       this.activities = [];
       this.clearSubTimers();
-      this.hours = this.minutes = this.seconds = "00";
+      this.hours = this.minutes = this.seconds = '00';
+      this.start_text = 'START';
     }
-    this.reset_text = "RESET";
-    this.start_text = "START OVER";
+    this.reset_text = 'RESET';
+
     this.changed = false;
     this.setCookies();
     this.cookieService.deleteAll();
   }
 
-  lapTimer(){
+  lapTimer() {
     let currentTime = this.timeElapsed;
     let val = new Date(currentTime);
-    val.setHours(val.getHours() - 5); 
+    val.setHours(val.getHours() - 5);
     val.setMinutes(val.getMinutes() - 30);
-    let name = this.pad(val.getHours()) + ' : ' + this.pad(val.getMinutes()) + ' : ' + this.pad(val.getSeconds());
+    let name =
+      this.pad(val.getHours()) +
+      ' : ' +
+      this.pad(val.getMinutes()) +
+      ' : ' +
+      this.pad(val.getSeconds());
     let hours = this.pad(val.getHours());
     let minutes = this.pad(val.getMinutes());
     let seconds = this.pad(val.getSeconds());
     let tag = '';
-    let lap = {name, hours, minutes, seconds, tag};
+    let lap = { name, hours, minutes, seconds, tag };
     this.laps.unshift(lap);
 
     // Set Cookie -
     let expiryDate = new Date();
-    expiryDate.setDate( expiryDate.getDate() + 1 );
+    expiryDate.setDate(expiryDate.getDate() + 1);
     this.cookieService.delete('main-laps');
     var json_str = JSON.stringify(this.laps);
     // console.log(this.laps);
     this.cookieService.set('main-laps', json_str, expiryDate);
   }
 
-
-  nameLap(i){
+  nameLap(i) {
     var lap_def = '';
-    if(this.laps[i].tag.length > 0){
+    if (this.laps[i].tag.length > 0) {
       lap_def = this.laps[i].tag;
     } else {
       lap_def = '';
     }
-    let lap_name: string = prompt("Customize the Lap Name - ", lap_def);
-    if(lap_name.length > 0){
+    let lap_name: string = prompt('Customize the Lap Name - ', lap_def);
+    if (lap_name.length > 0) {
       this.laps[i].tag = lap_name;
       let expiryDate = new Date();
-      expiryDate.setDate( expiryDate.getDate() + 1 );
+      expiryDate.setDate(expiryDate.getDate() + 1);
       this.cookieService.delete('main-laps');
       var json_str = JSON.stringify(this.laps);
       this.cookieService.set('main-laps', json_str, expiryDate);
     }
   }
 
-  pad(n){
+  pad(n) {
     return ('00' + n).substr(-2);
   }
 
@@ -184,9 +203,9 @@ export class AppComponent implements  OnInit, OnDestroy {
     this.timeElapsed = (this.timeElapsed || 0) + dt;
     // console.log(this.timeElapsed);
     let val = new Date(this.timeElapsed);
-    val.setHours(val.getHours()); 
+    val.setHours(val.getHours());
     val.setMinutes(val.getMinutes());
-    val.setHours(val.getHours() - 5); 
+    val.setHours(val.getHours() - 5);
     val.setMinutes(val.getMinutes() - 30);
     // console.log(val.getSeconds());
 
@@ -198,30 +217,30 @@ export class AppComponent implements  OnInit, OnDestroy {
     this.setCookies();
     this.saveTimerState();
   }
-  
-  initiate(h,m,n){
+
+  initiate(h, m, n) {
     this.hours = ('00' + h).substr(-2);
     this.minutes = ('00' + m).substr(-2);
     this.seconds = ('00' + n).substr(-2);
   }
 
-  closeLap(i){
+  closeLap(i) {
     this.laps.splice(i, 1);
     // Update Cookie -
     if (this.laps === undefined || this.laps.length == 0) {
       this.cookieService.delete('main-laps');
     } else {
       let expiryDate = new Date();
-      expiryDate.setDate( expiryDate.getDate() + 1 );
+      expiryDate.setDate(expiryDate.getDate() + 1);
       this.cookieService.delete('main-laps');
       var json_str = JSON.stringify(this.laps);
       this.cookieService.set('main-laps', json_str, expiryDate);
     }
   }
 
-  setCookies(){
+  setCookies() {
     let expiryDate = new Date();
-    expiryDate.setDate( expiryDate.getDate() + 1 );
+    expiryDate.setDate(expiryDate.getDate() + 1);
 
     this.cookieService.set('main-paused', this.paused, expiryDate);
     this.cookieService.set('main-lastUpdated', this.lastUpdated, expiryDate);
@@ -231,29 +250,28 @@ export class AppComponent implements  OnInit, OnDestroy {
     this.cookieService.set('main-hours', this.hours, expiryDate);
     this.cookieService.set('main-minutes', this.minutes, expiryDate);
     this.cookieService.set('main-seconds', this.seconds, expiryDate);
-    
   }
 
-  cookieCheck(){
-    if(this.cookieService.check('main-timeElapsed')) {
+  cookieCheck() {
+    if (this.cookieService.check('main-timeElapsed')) {
       this.timeElapsed = parseInt(this.cookieService.get('main-timeElapsed'));
     } else {
       this.timeElapsed = 0;
     }
 
-    if(this.cookieService.check('main-lastUpdated')) {
-      this.lastUpdated = (this.cookieService.get('main-lastUpdated'));
+    if (this.cookieService.check('main-lastUpdated')) {
+      this.lastUpdated = this.cookieService.get('main-lastUpdated');
     } else {
       // this.lastUpdated = 0;
     }
 
-    if(this.cookieService.check('main-paused')) {
+    if (this.cookieService.check('main-paused')) {
       this.paused = parseInt(this.cookieService.get('main-paused'));
     } else {
       this.paused = 0;
     }
 
-    if(this.cookieService.check('main-state')) {
+    if (this.cookieService.check('main-state')) {
       this.state = parseInt(this.cookieService.get('main-state'));
     } else {
       this.state = 0;
@@ -261,85 +279,101 @@ export class AppComponent implements  OnInit, OnDestroy {
 
     // Not much important shit
 
-    if(this.cookieService.check('main-hours')) {
+    if (this.cookieService.check('main-hours')) {
       this.hours = parseInt(this.cookieService.get('main-hours'));
     } else {
       this.hours = 0;
     }
 
-    if(this.cookieService.check('main-minutes')) {
+    if (this.cookieService.check('main-minutes')) {
       this.minutes = parseInt(this.cookieService.get('main-minutes'));
     } else {
       this.minutes = 0;
     }
 
-    if(this.cookieService.check('main-seconds')) {
+    if (this.cookieService.check('main-seconds')) {
       this.seconds = parseInt(this.cookieService.get('main-seconds'));
     } else {
       this.seconds = 0;
     }
   }
 
-  addTag(){
-    if(this.tag_name != ''){
+  addTag() {
+    if (this.tag_name != '') {
       let id = generate();
-      var colour = this.color_set1[Math.floor(Math.random() * this.color_set1.length)];
+      var colour = this.color_set1[
+        Math.floor(Math.random() * this.color_set1.length)
+      ];
       let activity = new ActivityListItem(id, colour, this.tag_name);
       this.activities.push(activity);
       // this.activities.push(this.tag_name);
-      for( var i = 0; i < this.color_set1.length; i++){ 
-        if ( this.color_set1[i] === colour) { 
-          this.color_set1.splice(i, 1); 
+      for (var i = 0; i < this.color_set1.length; i++) {
+        if (this.color_set1[i] === colour) {
+          this.color_set1.splice(i, 1);
         }
       }
       this.tag_name = '';
-      this.add_placeholder = "Another Task";
-    // console.log(this.activities);
+      this.add_placeholder = 'Another Task';
+      // console.log(this.activities);
       this.saveTimerState();
     }
   }
 
-  deleteTag(i){
+  deleteTag(i) {
     this.activities.splice(i, 1);
     this.saveTimerState();
     // Update Cookie -
     if (this.activities === undefined || this.activities.length == 0) {
-      this.add_placeholder = "Your Task";
+      this.add_placeholder = 'Your Task';
     }
   }
 
-  startSubTimer(i){
+  startSubTimer(i) {
     this.startTimer();
-    this.activities.forEach(activity => {
-      if(activity.getState()){
+    this.activities.forEach((activity) => {
+      if (activity.getState()) {
         activity.pauseTimer();
         activity.lastState = 0;
       }
-    })
+    });
     this.activities[i].startTimer();
     this.saveTimerState();
   }
 
-  fetchTimerState(): void{
+  fetchTimerState(): void {
     // var json_str = this.cookieService.get('main-timers');
     var json_str = localStorage.getItem('main-timers');
     let _activities = [];
     // cookie .length > 0
-    if(json_str){
+    if (json_str) {
       _activities = JSON.parse(json_str);
-      _activities.forEach(_activity => {
+      _activities.forEach((_activity) => {
         // Run the initiate code
-        let activity = new ActivityListItem(_activity.id, _activity.colour, _activity.name);
-        activity.fetchData(_activity.lastUpdated, _activity.timeElapsed, _activity.state, _activity.changed, _activity.reset, 
-                           _activity.lastState, _activity.paused, _activity.hours, _activity.minutes, _activity.seconds);
+        let activity = new ActivityListItem(
+          _activity.id,
+          _activity.colour,
+          _activity.name
+        );
+        activity.fetchData(
+          _activity.lastUpdated,
+          _activity.timeElapsed,
+          _activity.state,
+          _activity.changed,
+          _activity.reset,
+          _activity.lastState,
+          _activity.paused,
+          _activity.hours,
+          _activity.minutes,
+          _activity.seconds
+        );
         this.activities.push(activity);
 
-        this.add_placeholder = "Activities Set!";
+        this.add_placeholder = 'Activities Set!';
       });
     }
   }
 
-  saveTimerState(){
+  saveTimerState() {
     if (this.activities === undefined || this.activities.length == 0) {
       // this.cookieService.delete('main-timers');
       localStorage.removeItem('main-timers');
@@ -354,9 +388,9 @@ export class AppComponent implements  OnInit, OnDestroy {
     }
   }
 
-  clearSubTimers(){
+  clearSubTimers() {
     localStorage.removeItem('main-timers');
-    this.add_placeholder = "Your Task";
+    this.add_placeholder = 'Your Task';
     // this.cookieService.delete('main-timers');
   }
 }
